2ed75b872eaf300073323fe29b1390ab9fbed97e
messi2123 <91215240+messi2123@users.noreply.github.com>
2021-09-29 12:28:50 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+function bromista () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        `)
+}
+function amable () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+}
+function feliz2 () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        # . . . #
+        . # # # .
+        `)
+}
 basic.forever(function () {
 	
 })
