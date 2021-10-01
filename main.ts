e3bc1ed656ff964ff1420988d3ef994ce82578cf
messi2123 <91215240+messi2123@users.noreply.github.com>
2021-10-01 07:59:22 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
-function bromista () {
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        . . # . .
-        . # . # .
-        . . # . .
-        `)
-}
-function amable () {
+function AMABLE2 () {
     basic.showLeds(`
         . # . # .
         # # # # #
         # # # # #
         . # # # .
+        . . # . .
+        `)
+}
+function BROMISTA () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . # . .
+        . # . # .
         . . # . .
         `)
 }
@@ -25,6 +25,42 @@ function feliz2 () {
         . # # # .
         `)
 }
+let AMABLE = 0
+let LUZ = 0
+let AGUA = 0
 basic.forever(function () {
-	
+    let feliz = 0
+    if (feliz <= 0) {
+        basic.showString("(°-°)")
+        basic.showString("NO ")
+        basic.showString("HECHAR AGUA")
+    }
+    if (feliz <= 0) {
+        basic.showString("._.")
+        basic.showString("NO")
+        basic.showString("MOVER A UN LUGAR SOLEADO")
+    }
+    if (feliz <= 0) {
+        basic.showString(">_<")
+        basic.showString("NO")
+        basic.showString("ANIMARLA")
+        basic.pause(2000)
+        feliz2()
+    } else {
+        AMABLE()
+    }
+    if (AMABLE <= 0) {
+        basic.showString("\"_\"")
+        basic.showString("NO")
+        basic.showString("MOVER A UN LUGAR CON LUZ")
+    }
+    if (AMABLE <= 0) {
+        basic.showString("*_*")
+        basic.showString("NO ")
+        basic.showString("DARLE AGUA A LA PLANTA")
+    }
+    if (AMABLE <= 0) {
+        basic.showString("°<°")
+        basic.showString("NO")
+    }
 })
