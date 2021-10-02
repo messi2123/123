f7d4f8afca2a81c8fb65795d9049440d8ba9a400
messi2123 <91215240+messi2123@users.noreply.github.com>
2021-10-02 10:33:01 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -29,6 +29,7 @@ let AMABLE = 0
 let LUZ = 0
 let AGUA = 0
 basic.forever(function () {
+    let bromista = 0
     let feliz = 0
     if (feliz <= 0) {
         basic.showString("(°-°)")
@@ -45,11 +46,11 @@ basic.forever(function () {
         basic.showString("NO")
         basic.showString("ANIMARLA")
         basic.pause(2000)
-        feliz2()
     } else {
-        AMABLE()
+        feliz2()
     }
     if (AMABLE <= 0) {
+        AMABLE2()
         basic.showString("\"_\"")
         basic.showString("NO")
         basic.showString("MOVER A UN LUGAR CON LUZ")
@@ -62,5 +63,22 @@ basic.forever(function () {
     if (AMABLE <= 0) {
         basic.showString("°<°")
         basic.showString("NO")
+        basic.showString("tratarla bien")
+        BROMISTA()
+    }
+    if (bromista <= 0) {
+        basic.showString(":D")
+        basic.showString("NO")
+        basic.showString("CONTAR CHISTES")
+    }
+    if (bromista <= 0) {
+        basic.showString(";)")
+        basic.showString("NO")
+        basic.showString("HACERLA REIR")
+    }
+    if (bromista <= 0) {
+        basic.showString(":)")
+        basic.showString("NO")
+        basic.showString("HACER REIR A LOS DEMAS ")
     }
 })
