fc6af0a6c6f08b108b01e561c752609f0ab14293
messi2123 <91215240+messi2123@users.noreply.github.com>
2021-10-05 11:41:25 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,8 +6,25 @@ function AMABLE2 () {
         . # # # .
         . . # . .
         `)
+    if (AMABLE <= 0) {
+        basic.showString("[_]")
+        basic.showString("NO")
+        basic.showString(":%")
+    }
+    if (AMABLE <= 0) {
+        basic.showString("°0°")
+        basic.showString("NO ")
+        basic.showString("°_°")
+    }
+    if (AMABLE <= 0) {
+        basic.showString(":[")
+        basic.showString("NO")
+        basic.showString(":)")
+        BROMISTA()
+    }
 }
 function BROMISTA () {
+    let bromista = 0
     basic.showLeds(`
         . # . # .
         . . . . .
@@ -15,8 +32,24 @@ function BROMISTA () {
         . # . # .
         . . # . .
         `)
+    if (bromista <= 0) {
+        basic.showString("(!)")
+        basic.showString("NO")
+        basic.showString(":P")
+    }
+    if (bromista <= 0) {
+        basic.showString(":D")
+        basic.showString("NO")
+        basic.showString(":]")
+    }
+    if (bromista <= 0) {
+        basic.showString(":[")
+        basic.showString("NO")
+        basic.showString(":V")
+    }
 }
 function feliz2 () {
+    let feliz = 0
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -24,61 +57,24 @@ function feliz2 () {
         # . . . #
         . # # # .
         `)
+    if (feliz <= 0) {
+        basic.showString("[*+*]")
+        basic.showString("NO ")
+        basic.showString("#-#")
+    }
+    if (feliz <= 0) {
+        basic.showString("*+*")
+        basic.showString("NO")
+        basic.showString("*")
+    }
+    if (feliz <= 0) {
+        basic.showString("(°)")
+        basic.showString("NO")
+        basic.showString(":O")
+        AMABLE2()
+    }
 }
 let AMABLE = 0
+AMABLE = 0
 let LUZ = 0
 let AGUA = 0
-basic.forever(function () {
-    let bromista = 0
-    let feliz = 0
-    if (feliz <= 0) {
-        basic.showString("(°-°)")
-        basic.showString("NO ")
-        basic.showString("HECHAR AGUA")
-    }
-    if (feliz <= 0) {
-        basic.showString("._.")
-        basic.showString("NO")
-        basic.showString("MOVER A UN LUGAR SOLEADO")
-    }
-    if (feliz <= 0) {
-        basic.showString(">_<")
-        basic.showString("NO")
-        basic.showString("ANIMARLA")
-        basic.pause(2000)
-    } else {
-        feliz2()
-    }
-    if (AMABLE <= 0) {
-        AMABLE2()
-        basic.showString("\"_\"")
-        basic.showString("NO")
-        basic.showString("MOVER A UN LUGAR CON LUZ")
-    }
-    if (AMABLE <= 0) {
-        basic.showString("*_*")
-        basic.showString("NO ")
-        basic.showString("DARLE AGUA A LA PLANTA")
-    }
-    if (AMABLE <= 0) {
-        basic.showString("°<°")
-        basic.showString("NO")
-        basic.showString("tratarla bien")
-        BROMISTA()
-    }
-    if (bromista <= 0) {
-        basic.showString(":D")
-        basic.showString("NO")
-        basic.showString("CONTAR CHISTES")
-    }
-    if (bromista <= 0) {
-        basic.showString(";)")
-        basic.showString("NO")
-        basic.showString("HACERLA REIR")
-    }
-    if (bromista <= 0) {
-        basic.showString(":)")
-        basic.showString("NO")
-        basic.showString("HACER REIR A LOS DEMAS ")
-    }
-})
